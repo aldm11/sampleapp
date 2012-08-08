0b2ec8769a34a263eade7bc61d79624d735e97cc
aldm11 <amer_king88@hotmail.com>
2012-08-08 19:01:10 +0200

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,14 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   
   default_scope order: "microposts.created_at DESC"
+  
+  def self.from_users_followed_by(user)
+    #old code
+    # followed_user_ids = user.followed_user_ids
+    # where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    
+    followed_user_ids = "select followed_id from relationships where follower_id = :user_id"
+    where("user_id in (#{followed_user_ids}) or user_id = :user_id", user_id: user.id)
+    
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,13 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }  
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+  it { should respond_to(:followers) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -139,6 +146,28 @@ describe User do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
+    
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+    
   end
   
   describe "status" do
@@ -150,5 +179,31 @@ describe User do
     its(:feed) { should include(older_micropost) }
     its(:feed) { should_not include(unfollowed_post) }
   end
+  
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+    
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+    
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    
+  end
+  
+  
   
 end
